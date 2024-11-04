eb3072edc38ef1742377a32fbd390f0d8c17730a
Michael MacKenzie <michaelmackenzie@u.northwestern.edu>
2024-11-04 17:04:16 +0100

    Add more sub-groups, add observed processing option for envelope evaluation

--- tests/zemu_groups.py
+++ tests/zemu_groups.py
@@ -19,6 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--card", dest="card",default="datacard_prm_units_v5_total.txt", type=str,help="datacard name")
 parser.add_argument("--tag", dest="tag",default="test", type=str,help="output tag")
 parser.add_argument("--rrange", dest="rrange",default="5", type=str,help="POI r range")
+parser.add_argument("--obs", dest="obs",action='store_true', default=False,help="Run on the observed data instead of Asimov")
 parser.add_argument("--skip-fits", dest="skip_fits",action='store_true', default=False,help="Skip fits")
 args, unknown = parser.parse_known_args()
 if len(unknown)>0: 
@@ -27,32 +28,36 @@ if len(unknown)>0:
 
 
 groups = {
-    "envelope" : ["pdfindex_bin1", "pdfindex_bin2", "pdfindex_bin3"],
+    "envelope" : ["pdfindex_bin1", "pdfindex_bin2", "pdfindex_bin3"], # Effect is only visible in observed fits
     "params" : [],
     "IDs_trig" : ["idEleSF", "idMuSF", "isoEleSF", "isoMuSF", "dxyEleSF", "dxyMuSF", "recoEleSF", "triggersSF"],
-    "mu_scale" : ["mu_scale_bin1", "mu_scale_bin2", "mu_scale_bin3"],
-    "ele_scale" : ["ele_scale_bin1", "ele_scale_bin2", "ele_scale_bin3"],
+    "mu_scale" : ["nuisance_mu_scale_bin1", "nuisance_mu_scale_bin2", "nuisance_mu_scale_bin3"],
+    "ele_scale" : ["nuisance_ele_scale_bin1", "nuisance_ele_scale_bin2", "nuisance_ele_scale_bin3"],
     "stats" : ["statsBin1", "statsBin2", "statsBin3"],
     "zmumu_yield" : ["Zmm"],
     "jer_jes" : ["jer","jes"],
-    "scales" : ["bdt", "btagSF", "puSF", "prefire", "mixSF", "xsec", "zptSF", "pdf"],
+    "scales" : ["bdt", "btagSF", "puSF", "prefire", "mixSF", "xsec", "zptSF", "pdf", "lumi"],
+    "btag" : ["btagSF"],
+    "PU" : ["puSF"],
+    "theory" : ["pdf","mixSF"],
+    "lumi" : ["lumi"],
     "all" : [],
 }
 
 results = {}
 
 base_dir = os.getenv('CMSSW_BASE') + '/src/ZLFV_fits'
-
+obs_line = "" if args.obs else "-t -1"
 if not args.skip_fits:
     print ">>> Processing the nominal fit"
-    command = base_dir + '/tests/zemu_fit.sh ' + args.card + ' "-t -1" ' + args.tag + '_nominal ' + args.rrange
+    command = base_dir + '/tests/zemu_fit.sh ' + args.card + ' "' + obs_line + '" ' + args.tag + '_nominal ' + args.rrange
     os.system(command)
 results["nominal"] = extract_results(args.tag + '_nominal')
 
 for group in groups:
     params = groups[group]
     if not args.skip_fits:
-        command = base_dir + '/tests/zemu_fit.sh ' + args.card + ' "-t -1 --freezeParameters '
+        command = base_dir + '/tests/zemu_fit.sh ' + args.card + ' "' + obs_line + ' --freezeParameters '
         if group == "params":
             command += 'var{bkg_.*},var{ratio_bkg_.*},rgx{pdfindex_.*}'
         elif group == "all":
